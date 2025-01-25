eac411677b98cc1b62207a35adde8004c23ae86d
Rishi2600 <rishiraj6177@gmail.com>
2025-01-25 11:34:20 +0530

    improvised the previous step

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 import NavBar from "./(components)/NavBar"
-import { Providers } from "./provider"
 
 export default function Home() {
   return(
     <>
       <div>
-        <Providers>
         <NavBar />
-        </Providers>
       </div>
     </>
   )
