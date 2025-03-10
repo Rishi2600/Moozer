c3d00cc36366facdfd0bf1d3cb12da87ef728d68
Rishi2600 <rishiraj6177@gmail.com>
2025-03-10 21:28:44 +0530

    routing test

--- next-app/app/api/route.ts
+++ next-app/app/api/route.ts
@@ -1,4 +1,20 @@
 import { PrismaClient } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 const prisma = new PrismaClient();
 
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const user = await prisma.user.create({
+    data: {
+      name: "Alice",
+      email: "alice@prisma.io",
+    },
+  });
+  console.log(user);
+  res.json({
+    message: "added some data to the db for testing"
+  })
+}
