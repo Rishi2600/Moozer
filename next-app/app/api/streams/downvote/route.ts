3f43d6e49a3b62546afc63dea528d7430c54cc71
Rishi2600 <rishiraj6177@gmail.com>
2025-03-20 21:03:06 +0530

    added downvote endpoint

---
+++ next-app/app/api/streams/downvote/route.ts
@@ -0,0 +1,45 @@
+import { prismaClient } from "@/lib/db";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const CreateDownvoteSchema = z.object({
+    streamId: z.string()
+})
+
+export async function POST(req: NextRequest) {
+    //here we are checking the authentication of the user
+    const session = await getServerSession()
+    
+    const user = await prismaClient.user.findFirst({
+        where: {
+            email: session?.user?.email ?? ""
+        }
+    })
+
+    if(!user) {
+        return NextResponse.json({
+            message: "user not authenticated"
+        }, {
+            status: 403
+        })
+    }
+
+    try{
+        const data = CreateDownvoteSchema.parse(await req.json())
+        await prismaClient.upvote.delete({
+            where: {
+                UserId_StreamId: {
+                    UserId: user.id,
+                    StreamId: data.streamId,   
+                }
+            }
+        })
+    } catch(e) {
+        return NextResponse.json({
+            message: "error, cannot downvote"
+        }, {
+            status: 403
+        })
+    }
+}
