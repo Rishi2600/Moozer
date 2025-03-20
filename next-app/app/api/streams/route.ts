0827f631b3b70e445ad2f112736cacff941b718a
Rishi2600 <rishiraj6177@gmail.com>
2025-03-21 01:19:56 +0530

    endpoints added for now

--- next-app/app/api/streams/route.ts
+++ next-app/app/api/streams/route.ts
@@ -1,7 +1,11 @@
 import { z } from "zod";
 import { prismaClient } from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
-const YT_REGEX = new RegExp("/^(?:(?:https?:)?\/\/)?(?:www\.)?(?:m\.)?(?:youtu(?:be)?\.com\/(?:v\/|embed\/|watch(?:\/|\?v=))|youtu\.be\/)((?:\w|-){11})(?:\S+)?$/")
+//@ts-ignore
+import youtubesearchapi from "youtube-search-api";
+const YT_REGEX = new RegExp(
+    "/^(?:(?:https?:)?\/\/)?(?:www\.)?(?:m\.)?(?:youtu(?:be)?\.com\/(?:v\/|embed\/|watch(?:\/|\?v=))|youtu\.be\/)((?:\w|-){11})(?:\S+)?$/"
+)
 
 const CreateStreamSchema = z.object({
     creatorId: z.string(),
@@ -17,6 +21,8 @@ export async function POST (req: NextRequest) {
     try {
         const data = CreateStreamSchema.parse(await req.json());
         console.log(data.creatorId)
+
+        //this is done to find the extractedId & if the link belongs to a certain regex - yt.
         const isYt = data.url.match(YT_REGEX)
         if(!isYt) {
             NextResponse.json({
@@ -27,13 +33,30 @@ export async function POST (req: NextRequest) {
         }
 
         const extractedId = data.url.split("?v=")[1]
-        await prismaClient.stream.create({
+
+        const videoInfo = await youtubesearchapi.GetVideoDetails(extractedId);
+        console.log(videoInfo.title);
+        console.log(videoInfo.thumbnail.thumbnails);
+        const thumbnails = await videoInfo.thumbnail.thumbnails;
+
+        thumbnails.sort((a: {width: number}, b: {width: number}) => a.width < b.width ? -1 : 1)
+
+        const stream = await prismaClient.stream.create({
             data: {
                 userId: data.creatorId,
                 url: data.url,
                 extractedId: extractedId,
-                type: "Youtube"
+                type: "Youtube",
+                title: videoInfo.title ?? "SUIII",
+                bigImgUrl: thumbnails[thumbnails.length - 1].url ?? "",
+                smallImgUrl: (thumbnails.length > 1 ? thumbnails[thumbnails.length - 2].url : thumbnails[thumbnails.length - 1].url) ?? ""
+
             }
+        })
+
+        return NextResponse.json({
+            message: "stream added",
+            id: stream.id
         })
 
     }catch(e) {
