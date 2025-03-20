3bf599e95786219a1ce4d743820a864a563750f9
Rishi2600 <rishiraj6177@gmail.com>
2025-03-20 09:29:41 +0530

    added regex for youtube

--- next-app/app/api/streams/route.ts
+++ next-app/app/api/streams/route.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 import { prismaClient } from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
+const YT_REGEX = new RegExp("https?:\/\/(www\.)?youtube\.com\/watch\?v=[\w-]+(&[\w=-]+)*")
 
 const CreateStreamSchema = z.object({
     creatorId: z.string(),
@@ -16,8 +17,18 @@ export async function POST (req: NextRequest) {
     try {
         const data = CreateStreamSchema.parse(await req.json());
         console.log(data.creatorId)
-        
+        const isYt = YT_REGEX.test(data.url)
+        if(!isYt) {
+            NextResponse.json({
+                message: "not a youtube link"
+            }, {
+                status: 411
+            })
+        }
+
+        const extractedId = data.split        
         prismaClient.stream.create({
+            //@ts-ignore
             userId: data.creatorId,
             url: data.url,
             
