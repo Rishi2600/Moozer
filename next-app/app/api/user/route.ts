0f22782c1640e4d867333e0267f13af1e8fd2676
Rishi2600 <rishiraj6177@gmail.com>
2025-03-21 01:34:44 +0530

    user endpoint for extracting creatorId

---
+++ next-app/app/api/user/route.ts
@@ -0,0 +1,26 @@
+import { prismaClient } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+    const email = req.nextUrl.searchParams.get("email");
+
+    if (!email) {
+        return NextResponse.json({ 
+            message: "Email required" 
+        }, 
+        { 
+            status: 400 
+        });
+    }
+
+    const user = await prismaClient.user.findUnique({
+        where: { email }
+    });
+
+    if (!user) {
+        return NextResponse.json({ message: "User not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({ creatorId: user.id });
+}
+//this is done to extract the uuid at the first place.
