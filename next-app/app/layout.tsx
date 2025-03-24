b99a1c5b0c3d6f31622536971be6b917f31dd1f3
Rishi2600 <rishiraj6177@gmail.com>
2025-03-24 20:08:54 +0530

    solder structure altered

--- next-app/app/layout.tsx
+++ next-app/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./provider";
-import MusicStreamingDashboard from "./(components)/dashboard/Dashboard";
+import MusicStreamingDashboard from "./dashboard/page";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
